5ce34400d0c6e55d8165f942ff6d106c7562b5ea
Marcus Faustino <106195534+MarcusPauloFaustino1@users.noreply.github.com>
2022-06-19 17:38:33 -0300

    Add files via upload

--- semana05_aula01/ex12.py
+++ semana05_aula01/ex12.py
@@ -12,10 +12,10 @@ info = convert_to_dicionary(data)
 total_purchase = 0
 for l in info:
     year = l['ano']
-    age = l['compra']
+    age = l['idade']
     purchase = l['compra'] 
     
-    if age >= 18 and age <= 25 and year == 2010 or year == 2011 or year == 2012 or year == 2013 or year == 2014 or year == 2015:
+    if age >= 18 and age <= 25 and year >= 2010 and year <= 2015:
         total_purchase += purchase
 
 print(f'\n{" VALOR GASTO POR JOVENS DE 2010 ATÉ 2015 ":=^50}\n')     
